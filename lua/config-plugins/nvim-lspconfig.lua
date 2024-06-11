2c51ca1557c3ea8ea63107d642221abec9c5202c
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-06-11 19:24:59 +0200

    Add LaTeX language server

--- lua/config-plugins/nvim-lspconfig.lua
+++ lua/config-plugins/nvim-lspconfig.lua
@@ -2,7 +2,8 @@
     "neovim/nvim-lspconfig",
     config = function()
       -- require("vscode-html-languageserver").setup()
-      require'lspconfig'.pyright.setup{}
       require'lspconfig'.lua_ls.setup{}
+      require'lspconfig'.pyright.setup{}
+      require'lspconfig'.texlab.setup{}
     end
   }
