125d86424d3b72b2f813afb001748194e19f26d2
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-08-28 18:16:37 +0200

    Include more config

--- lua/config-plugins/noice.lua
+++ lua/config-plugins/noice.lua
@@ -2,10 +2,13 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
-    cmdline = {
-      view = "cmdline",
-    }
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
