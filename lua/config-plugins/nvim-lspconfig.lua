6fee2e993815cca918b00ac2c9035bba98b8d2d9
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-08-28 18:04:26 +0200

    Remove whitespace from lsp-config

--- lua/config-plugins/nvim-lspconfig.lua
+++ lua/config-plugins/nvim-lspconfig.lua
@@ -1,9 +1,9 @@
-  return {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- require("vscode-html-languageserver").setup()
-      require'lspconfig'.lua_ls.setup{}
-      require'lspconfig'.pyright.setup{}
-      require'lspconfig'.texlab.setup{}
-    end
-  }
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    -- require("vscode-html-languageserver").setup()
+    require'lspconfig'.lua_ls.setup{}
+    require'lspconfig'.pyright.setup{}
+    require'lspconfig'.texlab.setup{}
+  end
+}
