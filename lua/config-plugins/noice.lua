e4e8e4d3f03083eedfeed6f8a79e74adfa3505c1
qwrtln <qwrtln@users.noreply.github.com>
2024-09-06 09:08:03 +0200

    New style for lualine (#6)
    * New style for lualine
    
    * Adjust colors and formatting
    
    * Add credit

--- lua/config-plugins/noice.lua
+++ lua/config-plugins/noice.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   opts = {
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
+      bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
