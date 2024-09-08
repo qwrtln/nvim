396792fac1b14c1ea3820ba5f6774ccc42286654
qwrtln <qwrtln@users.noreply.github.com>
2024-09-08 22:42:16 +0200

    Fix lua ls (#13)
    * Remove annoying warning from lua_ls
    
    * Remove leftovers

--- lua/config-plugins/nvim-lspconfig.lua
+++ lua/config-plugins/nvim-lspconfig.lua
@@ -1,8 +1,21 @@
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    -- require("vscode-html-languageserver").setup()
-    require("lspconfig").lua_ls.setup {}
+    require("lspconfig").lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    }
     require("lspconfig").pyright.setup {}
     require("lspconfig").texlab.setup {}
   end,
