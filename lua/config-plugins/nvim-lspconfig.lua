71d109088e368e647fde375d44d30db01852c184
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-09-08 22:16:41 +0200

    Remove annoying warning from lua_ls

--- lua/config-plugins/nvim-lspconfig.lua
+++ lua/config-plugins/nvim-lspconfig.lua
@@ -2,7 +2,15 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     -- require("vscode-html-languageserver").setup()
-    require("lspconfig").lua_ls.setup {}
+    require("lspconfig").lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          }
+        }
+      }
+    }
     require("lspconfig").pyright.setup {}
     require("lspconfig").texlab.setup {}
   end,
