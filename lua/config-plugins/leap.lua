342616b54cf0e31964b9e72be0f94324ccc38105
Andrzej Wiącek <qwrtln@users.noreply.github.com>
2025-09-12 14:50:14 +0200

    Change default leap mapping s->f

--- lua/config-plugins/leap.lua
+++ lua/config-plugins/leap.lua
@@ -1,6 +1,7 @@
 return {
   "ggandor/leap.nvim",
   init = function()
-    require("leap").set_default_mappings()
+    vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap)")
+    vim.keymap.set("n", "F", "<Plug>(leap-from-window)")
   end,
 }
