8a1dbf8267024189774808f40c7a140f423bfb98
Andrzej Wiącek <qwrtln@users.noreply.github.com>
2025-10-20 00:22:58 +0200

    Move autocommands to respective plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,14 @@
+-- Get rid of inline messages from LSP
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+    vim.diagnostic.config {
+      virtual_text = false,
+    }
+  end,
+})
+
 local signs = { Error = "", Warn = "", Hint = "💡", Info = "" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,59 @@
+local language_parsers = {
+  "bash",
+  "css",
+  "comment",
+  "diff",
+  "dockerfile",
+  "go",
+  "hcl",
+  "html",
+  "javascript",
+  "json",
+  "latex",
+  "lua",
+  "markdown",
+  "perl",
+  "python",
+  "tmux",
+  "toml",
+  "typescript",
+  "yaml",
+}
+
+local function parser_to_filetype(parser)
+  local mappings = {
+    bash = "sh",
+    latex = "tex",
+  }
+  return mappings[parser] or parser
+end
+
+local language_filetypes = {}
+for _, parser in ipairs(language_parsers) do
+  table.insert(language_filetypes, parser_to_filetype(parser))
+end
+
+-- Start treesitter on selected file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = language_filetypes,
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
+-- custom parsers for treesitter
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").ghactions = {
+      install_info = {
+        url = "https://github.com/rmuir/tree-sitter-ghactions",
+        queries = "queries",
+      },
+    }
+  end,
+})
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,26 +61,7 @@ return {
     branch = "main",
     build = ":TSUpdate",
     init = function()
-      require("nvim-treesitter").install {
-        "bash",
-        "css",
-        "comet",
-        "diff",
-        "dockerfile",
-        "go",
-        "hcl",
-        "html",
-        "javascript",
-        "json",
-        "latex",
-        "lua",
-        "markdown",
-        "perl",
-        "python",
-        "toml",
-        "typescript",
-        "yaml",
-      }
+      require("nvim-treesitter").install(language_parsers)
     end,
   },
 }
