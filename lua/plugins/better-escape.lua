9aa37e3622cf7e6e46ac5eb4cfd592a20d549756
Andrzej Wiącek <qwrtln@users.noreply.github.com>
2025-11-06 17:04:49 +0100

    Add better escape

---
+++ lua/plugins/better-escape.lua
@@ -0,0 +1,7 @@
+return {
+  "max397574/better-escape.nvim",
+  opts = {
+    timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
+    default_mappings = true, -- setting this to false removes all the default mappings
+  },
+}
