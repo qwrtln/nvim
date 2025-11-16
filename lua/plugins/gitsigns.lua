3b3e8bd9ba3ade2c872476785e2c9fe666d20160
qwrtln <qwrtln@users.noreply.github.com>
2025-11-16 22:59:45 +0100

    Add reset hunk func (#57)

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 
 map("n", "<C-s>", "<cmd>Gitsigns preview_hunk<CR>")
+vim.api.nvim_create_user_command("Rh", "Gitsigns reset_hunk", {
+  desc = "Reset hunk",
+})
 
 return {
   "lewis6991/gitsigns.nvim",
