c1fc2830058ad74eccbb53bfa19fd4fab5f026c0
Andrzej Wiącek <qwrtln@users.noreply.github.com>
2026-01-28 23:32:05 +0100

    Disable AI, update leap URL

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,5 +1,5 @@
 return {
-  "ggandor/leap.nvim",
+  url = "https://codeberg.org/andyg/leap.nvim",
   init = function()
     vim.keymap.set({ "n", "x", "o" }, "m", "<Plug>(leap)")
     vim.keymap.set("n", "M", "<Plug>(leap-from-window)")
